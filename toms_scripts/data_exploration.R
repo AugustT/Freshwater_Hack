79766aa58d6cabae09a9f8e3988b2a23359d51ed
Tom August <tomaug@ceh.ac.uk>
2017-05-25 21:43:23 +0100

    minor

--- toms_scripts/data_exploration.R
+++ toms_scripts/data_exploration.R
@@ -6,6 +6,7 @@ summary(raw_data)
 
 # What do the last dates of activity look like
 raw_data$Latest <- as.Date(as.character(raw_data$Latest), format = '%d-%b-%y')
+
 hist(raw_data$Latest[raw_data$Latest > as.Date('2011-01-01')], breaks = 'weeks')
 
 table(raw_data$Latest)
