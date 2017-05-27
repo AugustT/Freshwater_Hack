ab640f576af95bc432da4e774f555bc2b3974ae8
hjrobson2 <hannah.robson@wwt.org.uk>
2017-05-27 09:10:51 +0100

    Previous script included lots of bits of me playing around with different visualisations -cleaner version

--- Hannah_Script/HJR_script.R
+++ Hannah_Script/HJR_script.R
@@ -3,24 +3,42 @@ EngDataTime<-read.csv(file.choose())
 EngDataSum<-read.csv(file.choose())
 summary(EngDataSum)
 summary(EngDataTime)
+summary(CollData)
+
 #################################
 
-library(ggplot2)
-library(scales)
-library(corrplot)
-
-dframe3<-read.csv(file.choose())
-
-M<-cor(dframe3)
-head(round(M,2))
-
-corrplot(M, method="circle")
-
-
-####################################################
-
-EngDataSum<-read.csv(file.choose())
-
-model1<-glm(Sample_bi~Attendees+Paid+TranTrai+Upload+access, family="binomial", data=EngDataSum)
+model1<-glm(Sample_bi~Paid+TranTrai+Upload+Attendees, family="binomial", data=EngDataSum)
 summary(model1)
 
+model2<-glm(Super_user~PCA1_Difficulty+Upload+Team+People,family="binomial",data=EngDataSum)
+summary(model2)
+
+
+############################################################################################
+
+#playing around with visualising models#
+library(visreg)
+
+visreg(model1,xvar="Attendees")
+visreg(model1,xvar="Paid")
+visreg(model1,xvar="TranTrai")
+visreg(model1,xvar="Upload")
+
+
+x<-table(EngDataSum$Sample_bi,EngDataSum$Paid)
+mosaicplot(x,data=EngDataSum,color=2:4,legend=TRUE,las = 1)
+
+
+x<-read.csv(file.choose())
+
+
+library(reshape2)
+x <- melt(x)
+names(x) <- c("Paid","Stay","value")
+
+library(ggplot2)
+ggplot(x, aes(Paid, Stay)) +
+  geom_point(aes(size = value), alpha=0.8, color="darkgreen", show_guide=FALSE) +
+  geom_text(aes(label = value), color="white") +
+  scale_size(range = c(15,50)) +
+  theme_bw()+xlim(-0.5,1.5)+ylim(-0.5,1.5)
